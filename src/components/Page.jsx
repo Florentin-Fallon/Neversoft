7060a3c22c57661e6c30188781744c9bec403ad7
FlorentinFallon <119119355+FlorentinFallon@users.noreply.github.com>
2023-12-13 13:43:47 +0100

    create animation for landing page

--- src/components/Page.jsx
+++ src/components/Page.jsx
@@ -1,11 +1,23 @@
 import React from "react";
+import Typed from 'react-typed';
 
 const Page = () => {
     return ( 
     <div className="text-white">
         <div className="max-w-[800px] mt-[-96px] w-full h-screen mx-auto text-center flex flex-col justify-center">
             <h1 className="bg-gradient-to-r from-white from-2% via-red-400 to-100% inline-block text-transparent bg-clip-text md:text-6xl sm:text-6xl text-4xl font-bold md:py-6">Bienvenue chez Neversoft</h1>
-            <p className="p-4 text-xl">"Unis par la passion, guidés par l'adrénaline"</p>
+            <div className="flex justify-center items-center">
+                <p className=" md:text-xl sm:text-xl text-xl">
+                    Unis par la passion, guidés par
+                </p>
+                <Typed
+                className=" md:text-xl sm:text-xl text-xl p-2"
+                strings={["l'adrénaline","l'émotion", "l'énergie", "les frissons", "la vitesse", "la compétition"]}
+                typeSpeed={120}
+                backSpeed={140}
+                loop
+                />
+            </div>
         </div>
     </div>
     )
