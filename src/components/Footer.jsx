854e09303c9b2e9dd11d7ad83a6a9d915dd11ffe
FlorentinFallon <119119355+FlorentinFallon@users.noreply.github.com>
2024-01-04 10:27:35 +0100

    create footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,26 @@
-import React from "react";
+import React from 'react';
+import {
+  FaFacebookSquare,
+  FaInstagram,
+  FaTiktok
+} from 'react-icons/fa';
 
 const Footer = () => {
-    return (
+  return (
+    <div className='max-w-[1240px] flex mx-auto pt-16 pb-4 px-4 mt-24 lg:grid-cols-3 gap-8 text-white text-center justify-center'>
         <div>
-            <h1 className="text-white">Footer</h1>
+            <h1 className='w-full text-3xl font-bold text-white'>Neversoft</h1>
+            <p className='text-center pt-4'>Bienvenue chez Neversoft, le club d'airsoft où la passion et l'action se rejoignent pour créer des moments inoubliables. 
+            Plongez dans notre communauté dédiée à l'excitation tactique et à la camaraderie.
+            </p>
+                <div className='flex md:w-[75%] my-6 justify-center mx-36'>
+                    <FaFacebookSquare className='pl-4' size={35} />
+                    <FaInstagram className='pl-4' size={35} />
+                    <FaTiktok className='pl-4' size={35} />
+                </div>
         </div>
-    )
-}
+    </div>
+  );
+};
 
 export default Footer;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -20,9 +20,9 @@ const Navbar = () => {
                 <li className="p-4">Contact</li>
             </ul>
             <div onClick={handleNav} className="block md:hidden">
-                {!nav ? <AiOutlineClose size={20} /> : <AiOutlineMenu size={20} />}
+                {nav ? <AiOutlineClose size={20} /> : <AiOutlineMenu size={20} />}
             </div>
-            <div className={!nav ? "fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300] ease-in-out duration-500 " : "fixed left-[-100%]"}>
+            <div className={nav ? "fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300] ease-in-out duration-500 " : "fixed left-[-100%]"}>
             <h1 className="w-full text-3xl font-bold text-[white] m-4">Neversoft</h1>
                 <ul className="uppercase p-4">
                 <li className="p-4 border-b border-gray-600">Accueil</li>
