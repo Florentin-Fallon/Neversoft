94690fc4193fe06f1f8c00e2e54848c64446c5f3
FlorentinFallon <119119355+FlorentinFallon@users.noreply.github.com>
2024-01-04 13:09:05 +0100

    modified App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Home from './Home';
 import Team from './Team';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Inscription from './Inscription';
 import Tarifs from './Tarifs';
 import Boutique from './Boutique';
