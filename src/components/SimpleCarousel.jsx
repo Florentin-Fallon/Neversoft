a7967695232585477e8a3d78923c2f7793138f03
FlorentinFallon <119119355+FlorentinFallon@users.noreply.github.com>
2024-02-17 20:22:48 +0100

    modified card image partenaire

--- src/components/SimpleCarousel.jsx
+++ src/components/SimpleCarousel.jsx
@@ -17,11 +17,11 @@ function SimpleCarousel({
     <div>
       <h1 className='text-white text-center text-3xl mt-16 mb-24'>{title}</h1>
         <div className='flex flex-wrap justify-center'>
-        <img src={imgSrc} alt={imgAlt} className='w-64 h-64 bg-no-repeat rounded-xl transition duration-300 transform hover:scale-110'/>
-        <img src={imgSrc2} alt={imgAlt2} className='w-64 h-64 bg-no-repeat ml-4 bg-cover rounded-xl transition duration-300 transform hover:scale-110'/>
-        <img src={imgSrc3} alt={imgAlt3} className='w-64 h-64 bg-no-repeat ml-4 rounded-xl transition duration-300 transform hover:scale-110'/>
-        <img src={imgSrc4} alt={imgAlt4} className='w-64 h-64 bg-no-repeat ml-4 rounded-xl transition duration-300 transform hover:scale-110'/>
-        <img src={imgSrc5} alt={imgAlt5} className='w-64 h-64 bg-no-repeat ml-4 rounded-xl transition duration-300 transform hover:scale-110'/>
+        <img src={imgSrc} alt={imgAlt}   className='w-64 h-64 bg-no-repeat ml-4 mb-4 rounded-xl transition duration-300 transform hover:scale-110'/>
+        <img src={imgSrc2} alt={imgAlt2} className='w-64 h-64 bg-no-repeat ml-4 mb-4 rounded-xl transition duration-300 transform hover:scale-110'/>
+        <img src={imgSrc3} alt={imgAlt3} className='w-64 h-64 bg-no-repeat ml-4 mb-4 rounded-xl transition duration-300 transform hover:scale-110'/>
+        <img src={imgSrc4} alt={imgAlt4} className='w-64 h-64 bg-no-repeat ml-4 mb-4 rounded-xl transition duration-300 transform hover:scale-110'/>
+        <img src={imgSrc5} alt={imgAlt5} className='w-64 h-64 bg-no-repeat ml-4 mb-4 rounded-xl transition duration-300 transform hover:scale-110'/>
     </div>
     </div>
   )
