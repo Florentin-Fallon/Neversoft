acd30e66c3ab293f195e6126b1929e389887b3ac
FlorentinFallon <119119355+FlorentinFallon@users.noreply.github.com>
2024-02-24 16:18:22 +0100

    create branch and switch to branch

--- src/Contact.js
+++ src/Contact.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
-import Maintenance from './components/Maintenance'
 
 function Contact() {
   return (
@@ -9,11 +8,11 @@ function Contact() {
         <div>
             <Navbar />
         </div>
-        <div>
-          <Maintenance 
-          text={'La page contact'} 
-          />
-        </div>
+        <div className="text-center text-white w-full mt-24">
+        <h1 className="text-4xl font-bold mb-8">Contactez-nous !</h1>
+        <p className="text-lg mb-4">Vous pouvez nous contacter par e-mail à l'adresse suivante :</p>
+        <a href="mailto:adresse@mail.com" className="text-blue-300 hover:text-blue-100 underline">club.neversoft@gmail.com</a>
+      </div>
         <div>
         <Footer />
       </div>
