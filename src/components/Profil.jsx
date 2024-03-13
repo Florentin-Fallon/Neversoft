03f43191a497e7940909eae0eab4053babae66ef
FlorentinFallon <119119355+FlorentinFallon@users.noreply.github.com>
2024-03-13 10:15:26 +0100

    modified component profil

--- src/components/Profil.jsx
+++ src/components/Profil.jsx
@@ -7,7 +7,7 @@ const Profil = ({
     description,
 }) => {
   return (
-    <div className='bg-white flex flex-col border-solid w-[17rem] h-[22rem] rounded-lg m-px mt-16'>
+    <div className='bg-white flex flex-col border-solid w-[17rem] h-[22rem] rounded-lg m-px mt-16 transition duration-300 transform hover:scale-105'>
         <img className='rounded-t-lg w-[64] h-64' src={imgS} alt={imgA} />
         <h1 className='mt-2 text-xl text-center font-semibold'>{title}</h1>
         <p className='text-center text-sm w-[50%] m-auto mt-2 mb-4 '>{description}</p>
